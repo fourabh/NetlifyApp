5899bb139ea748b2e8893c722e6720a29d463e3b
sourabh <raisourabh85@gmail.com>
2022-09-21 14:01:47 +0530

    Updates

---
+++ precache-manifest.e5f08a2c8376128df4608f4c70d89163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e116e64658e3149091c",
+    "url": "/Text_Analizer/static/css/main.cbc7668c.chunk.css"
+  },
+  {
+    "revision": "0e116e64658e3149091c",
+    "url": "/Text_Analizer/static/js/main.0e116e64.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/Text_Analizer/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/Text_Analizer/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "ae340adc7ffa3f6cfe1c",
+    "url": "/Text_Analizer/static/js/runtime~main.ae340adc.js"
+  },
+  {
+    "revision": "4efecec571c1908aba30e2b282a99a1e",
+    "url": "/Text_Analizer/index.html"
+  }
+];
